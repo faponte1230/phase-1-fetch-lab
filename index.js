3237c8144dbc8f12ef1138434cac26d30817c5c7
Fernando Aponte <faponte1230@yahoo.com>
2022-12-04 19:35:25 -0500

    commit tree ec3237a03aa7ebb3c9265ede6828f92ffd50491a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -11,7 +12,6 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
